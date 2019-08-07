b531859864c4b2de5469f19b571f0973d47fd691
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-07 14:21:53 -0300

    tela inicial estilizada

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./Main.css";
 
-export default function Main() {
-  return <h1>Main</h1>;
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+
+export default function Main({ match }) {
+  useEffect(() => {}, []);
+
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt=""
+          />
+          <footer>
+            <strong>Felipe Dechamps</strong>
+            <p>Programador e cantor</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt=""
+          />
+          <footer>
+            <strong>Felipe Dechamps</strong>
+            <p>Programador e cantor</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt=""
+          />
+          <footer>
+            <strong>Felipe Dechamps</strong>
+            <p>Programador e cantor</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt=""
+          />
+          <footer>
+            <strong>Felipe Dechamps</strong>
+            <p>Programador e cantor</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }

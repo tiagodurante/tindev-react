a446a503fc5aba1f1893c3f06013df151064b49b
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-07 13:43:08 -0300

    testando states no react

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Login.css";
 import logo from "../assets/logo.svg";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(username);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
-        <input placeholder="digite seu usuário do github" />
-        <button value="Enviar" submit="enviar" />
+        <input
+          placeholder="digite seu usuário do github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Main from "./pages/Main";
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Login} />
+      <Route path="/" exact component={Login} />
       <Route path="/main" component={Main} />
     </BrowserRouter>
   );
